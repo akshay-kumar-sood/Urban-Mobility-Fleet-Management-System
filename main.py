d6d67b929f7c050f01225ba5955b8884896c4ef6
Akshay Kumar Sood <id.akshay23@gmail.com>
2026-08-04 12:22:53 +0530

    update :[AKSHAY] UC1 : Basic Vehicle Implementation

---
+++ main.py
@@ -0,0 +1,6 @@
+from Vehicle import Vehicle
+
+v1=Vehicle(1051,2025,90)
+print(f"Vehicle id is : {v1.vehicle_id}")
+print(f"Vehicle model is : {v1.model}")
+print(f"Vehicle battery percentage is : {v1.battery_percentage} %")
