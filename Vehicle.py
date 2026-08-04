ae07cf93e7d6a4ea77294446dce4bfc972682147
Akshay Kumar Sood <id.akshay23@gmail.com>
2026-08-04 12:47:18 +0530

    update :[AKSHAY] UC2-Encapsulation & Security

--- Vehicle.py
+++ Vehicle.py
@@ -1,7 +1,34 @@
 class Vehicle:
-    def __init__(self,vehicle_id,model,battery_percentage):
-        self.vehicle_id=vehicle_id
-        self.model=model
-        self.battery_percentage=battery_percentage
 
+    def __init__(self, vehicle_id, model, battery_percentage,maintenance_status, rental_price):
 
+        self.vehicle_id = vehicle_id
+        self.model = model
+        self.battery_percentage = None
+        self.set_battery_percentage(battery_percentage)
+        self.__maintenance_status = maintenance_status
+        self.__rental_price = rental_price
+
+    
+    def get_battery_percentage(self):
+        return self.battery_percentage
+
+    def set_battery_percentage(self, battery_percentage):
+        if 0 <= battery_percentage <= 100:
+            self.battery_percentage = battery_percentage
+        else:
+            raise ValueError("Battery percentage must be between 0 and 100.")
+
+    
+    def get_maintenance_status(self):
+        return self.__maintenance_status
+
+    def set_maintenance_status(self, maintenance_status):
+        self.__maintenance_status = maintenance_status
+
+    
+    def get_rental_price(self):
+        return self.__rental_price
+
+    def set_rental_price(self, rental_price):
+        self.__rental_price = rental_price
--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from Vehicle import Vehicle
 
-v1=Vehicle(1051,2025,90)
-print(f"Vehicle id is : {v1.vehicle_id}")
-print(f"Vehicle model is : {v1.model}")
-print(f"Vehicle battery percentage is : {v1.battery_percentage} %")
+v1 = Vehicle(
+    1051,
+    "Tesla Model 3",
+    90,
+    "Available",
+    250000
+)
+
+print(f"Vehicle ID               : {v1.vehicle_id}")
+print(f"Vehicle Model            : {v1.model}")
+print(f"Battery Percentage       : {v1.get_battery_percentage()} %")
+print(f"Maintenance Status       : {v1.get_maintenance_status()}")
+print(f"Rental Price             : ₹{v1.get_rental_price()}")
+
+print("\nAfter Updating Data\n")
+
+v1.set_battery_percentage(80)
+v1.set_maintenance_status("Under Maintenance")
+v1.set_rental_price(350000)
+
+print(f"Vehicle ID               : {v1.vehicle_id}")
+print(f"Vehicle Model            : {v1.model}")
+print(f"Battery Percentage       : {v1.get_battery_percentage()} %")
+print(f"Maintenance Status       : {v1.get_maintenance_status()}")
+print(f"Rental Price             : ₹{v1.get_rental_price()}")
